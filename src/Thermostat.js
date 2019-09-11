c66fcc7965e8576a987378b374266ebb2f267e35
gcheval <chevalier321@gmail.com>
2019-09-11 18:48:04 -0400

    syntax error fix

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -66,7 +66,7 @@ module.exports = {
                                 return;
                             }
                             this.service.setCharacteristic(Characteristic.CurrentTemperature, temperature);
-                            this.service.setCharacteristic((Characteristic.CurrentRelativeHumidity, humidity);
+                            this.service.setCharacteristic(Characteristic.CurrentRelativeHumidity, humidity);
                         })
                     }, this.pollingInterval)
                 }
